87bc47eccf98824708ead7ad80c1476dc237c4a8
dindayal1997 <108941910+dindayal1997@users.noreply.github.com>
2022-08-06 14:52:58 +0530

    Assignment 4 @ Edyoda

---
+++ Assignment 4.3.py
@@ -0,0 +1,18 @@
+# pythone programm to squre the element of alist
+# using Map() function
+
+
+lst = [10,7,3,5,12]
+
+def squre (lst):
+    return lst**2
+
+data = list(map(squre,lst))
+
+print(data)
+
+
+
+
+
+
